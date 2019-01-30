d090c9acf986826d2e21afbc85e0048252d45d5d
Teodor Dermendzhiev <Teodor.Dermendzhiev@progress.com>
2019-01-30 12:24:31 +0200

    Add -p flags to cmake-build commands

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ tar xzf clang+llvm-7.0.0-x86_64-apple-darwin.tar.xz -C ./7.0.0
 patch ./4.0.1/clang+llvm-4.0.1-x86_64-apple-macosx10.9.0/include/clang/AST/RecursiveASTVisitor.h ./ast-visitor-4.0.1.patch
 patch ./7.0.0/clang+llvm-7.0.0-x86_64-apple-darwin/include/clang/AST/RecursiveASTVisitor.h ./ast-visitor-7.0.0.patch
 cd 4.0.1
-mkdir cmake-build && cd cmake-build
+mkdir -p cmake-build && cd cmake-build
 cmake .. -GXcode
 cd ../../7.0.0
-mkdir cmake-build && cd cmake-build
+mkdir -p cmake-build && cd cmake-build
 cmake .. -GXcode
